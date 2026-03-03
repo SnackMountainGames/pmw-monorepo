d474a592251bf479e18460f8b97a8c00c5037b5a
Kevin Claus <kevin.claus@gmail.com>
2026-03-02 23:42:42 -0800

    Added emotion styling

--- apps/phone-client/src/components/hud/Hud.tsx
+++ apps/phone-client/src/components/hud/Hud.tsx
@@ -1,24 +1,24 @@
 import { useGameStore } from '../../state/GameState';
+import styled from '@emotion/styled';
+
+const TopHud = styled.div`
+    position: absolute;
+    left: 0;
+    right: 0;
+    box-sizing: border-box;
+    pointer-events: none;
+    touch-action: none;
+    user-select: none;
+    display: flex;
+    justify-content: center;
+    margin: 20;
+`;
 
 export const Hud = () => {
   const { roomCode, name } = useGameStore();
 
   return (
-    <div
-      id="game-hud"
-      style={{
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        boxSizing: 'border-box',
-        pointerEvents: 'none',
-        touchAction: 'none',
-        userSelect: 'none',
-        display: 'flex',
-        justifyContent: 'center',
-        margin: 20,
-      }}
-    >
+    <TopHud id="game-hud">
       {roomCode ? (
         <h2>
           Successfully joined room {roomCode} as {name} 🎉
@@ -26,6 +26,6 @@ export const Hud = () => {
       ) : (
         <h2>⚠ Not actually in a room (testing) ⚠</h2>
       )}
-    </div>
+    </TopHud>
   );
 }
